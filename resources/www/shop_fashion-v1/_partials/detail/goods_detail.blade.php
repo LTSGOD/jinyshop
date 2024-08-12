694122ab1af13fbb7d79151860188ffb8dfe9e93
hojinlee <infohojin@gmail.com>
2024-08-12 19:44:33 +0900

    shop detail info

---
+++ resources/www/shop_fashion-v1/_partials/detail/goods_detail.blade.php
@@ -0,0 +1,142 @@
+<!-- Reviews -->
+                <a class="d-none d-md-flex align-items-center gap-2 text-decoration-none mb-3" href="#reviews">
+                    <div class="d-flex gap-1 fs-sm">
+                        <i class="ci-star-filled text-warning"></i>
+                        <i class="ci-star-filled text-warning"></i>
+                        <i class="ci-star-filled text-warning"></i>
+                        <i class="ci-star-filled text-warning"></i>
+                        <i class="ci-star text-body-tertiary opacity-75"></i>
+                    </div>
+                    <span class="text-body-tertiary fs-sm">23 reviews</span>
+                </a>
+
+                <!-- Title -->
+                <h1 class="h3">Denim midi skirt with pockets</h1>
+
+                <!-- Description -->
+                <p class="fs-sm mb-0">Made from high-quality denim fabric, this midi skirt offers durability and
+                    comfort for all-day wear. The mid-length design strikes the perfect balance between casual and
+                    chic, making it suitable for various occasions, from casual outings to semi-formal events.</p>
+                <div class="collapse" id="moreDescription">
+                    <div class="fs-sm pt-3">
+                        <p>One of the standout features of this skirt is its functional pockets. With two spacious
+                            pockets at the front, you can conveniently carry your essentials such as keys, phone, or
+                            wallet without the need for a bulky bag. The pockets also add a touch of utility and
+                            flair to the overall look.</p>
+                        <p class="mb-0">The skirt's classic denim color and timeless design make it easy to pair
+                            with a variety of tops, blouses, and footwear, allowing you to create endless stylish
+                            ensembles. Whether you prefer a laid-back look with a graphic tee and sneakers or a more
+                            polished ensemble with a blouse and heels, this skirt effortlessly adapts to your style.
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Price -->
+                <a class="d-inline-block fs-sm fw-medium text-dark-emphasis collapsed mt-1" href="#moreDescription"
+                    data-bs-toggle="collapse" aria-expanded="false" aria-controls="moreDescription"
+                    data-label-collapsed="Read more" data-label-expanded="Show less"
+                    aria-label="Show / hide description"></a>
+                <div class="h4 d-flex align-items-center my-4">
+                    $126.50
+                    <del class="fs-sm fw-normal text-body-tertiary ms-2">$156.00</del>
+                </div>
+
+                <!-- Color options -->
+                <div class="mb-4">
+                    <label class="form-label fw-semibold pb-1 mb-2">Color: <span class="text-body fw-normal"
+                            id="colorOption">Dark blue</span></label>
+                    <div class="d-flex flex-wrap gap-2" data-binded-label="#colorOption">
+                        <input type="radio" class="btn-check" name="colors" id="dark-blue" checked>
+                        <label for="dark-blue" class="btn btn-image p-0" data-label="Dark blue">
+                            <img src="/assets/img/shop/fashion/product/colors/color01.png" width="56"
+                                alt="Dark blue">
+                            <span class="visually-hidden">Dark blue</span>
+                        </label>
+                        <input type="radio" class="btn-check" name="colors" id="pink">
+                        <label for="pink" class="btn btn-image p-0" data-label="Pink">
+                            <img src="/assets/img/shop/fashion/product/colors/color02.png" width="56"
+                                alt="Pink">
+                            <span class="visually-hidden">Pink</span>
+                        </label>
+                        <input type="radio" class="btn-check" name="colors" id="light-blue">
+                        <label for="light-blue" class="btn btn-image p-0" data-label="Light blue">
+                            <img src="/assets/img/shop/fashion/product/colors/color03.png" width="56"
+                                alt="Light blue">
+                            <span class="visually-hidden">Light blue</span>
+                        </label>
+                    </div>
+                </div>
+
+                <!-- Size select -->
+                <div class="mb-3">
+                    <div class="d-flex align-items-center justify-content-between mb-1">
+                        <label class="form-label fw-semibold mb-0">Size</label>
+                        <div class="nav">
+                            <a class="nav-link animate-underline fw-normal px-0" href="#sizeGuide"
+                                data-bs-toggle="modal">
+                                <i class="ci-ruler fs-lg me-2"></i>
+                                <span class="animate-target">Size guide</span>
+                            </a>
+                        </div>
+                    </div>
+                    <select class="form-select form-select-lg"
+                        data-select='{
+              "classNames": {
+                "containerInner": "form-select form-select-lg"
+              }
+            }'
+                        aria-label="Material select">
+                        <option value="">Choose a size</option>
+                        <option value="xs">6-8 (XS)</option>
+                        <option value="s">8-10 (S)</option>
+                        <option value="m">10-12 (M)</option>
+                        <option value="l">12-14 (L)</option>
+                        <option value="xl">14-16 (XL)</option>
+                    </select>
+                </div>
+
+                <!-- Count input + Add to cart button -->
+                <div class="d-flex gap-3 pb-3 pb-lg-4 mb-3">
+                    <div class="count-input flex-shrink-0">
+                        <button type="button" class="btn btn-icon btn-lg" data-decrement
+                            aria-label="Decrement quantity">
+                            <i class="ci-minus"></i>
+                        </button>
+                        <input type="number" class="form-control form-control-lg" min="1" value="1"
+                            readonly>
+                        <button type="button" class="btn btn-icon btn-lg" data-increment
+                            aria-label="Increment quantity">
+                            <i class="ci-plus"></i>
+                        </button>
+                    </div>
+                    <button type="button" class="btn btn-lg btn-dark w-100">Add to cart</button>
+                </div>
+
+                <!-- Info list -->
+                <ul class="list-unstyled gap-3 pb-3 pb-lg-4 mb-3">
+                    <li class="d-flex flex-wrap fs-sm">
+                        <span class="d-flex align-items-center fw-medium text-dark-emphasis me-2">
+                            <i class="ci-clock fs-base me-2"></i>
+                            Estimated delivery:
+                        </span>
+                        15 - 27 Mar, 2024
+                    </li>
+                    <li class="d-flex flex-wrap fs-sm">
+                        <span class="d-flex align-items-center fw-medium text-dark-emphasis me-2">
+                            <i class="ci-delivery fs-base me-2"></i>
+                            Free shipping &amp; returns:
+                        </span>
+                        On all orders over $100.00
+                    </li>
+                </ul>
+
+                <!-- Stock status -->
+                <div class="d-flex flex-wrap justify-content-between fs-sm mb-3">
+                    <span class="fw-medium text-dark-emphasis me-2">🔥 Hurry up! The sale is coming to an
+                        end</span>
+                    <span><span class="fw-medium text-dark-emphasis">6</span> items in stock!</span>
+                </div>
+                <div class="progress" role="progressbar" aria-label="Left in stock" aria-valuenow="25"
+                    aria-valuemin="0" aria-valuemax="100" style="height: 4px">
+                    <div class="progress-bar rounded-pill" style="width: 25%"></div>
+                </div>
